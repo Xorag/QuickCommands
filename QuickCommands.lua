976e6a9455af813e64a9d9393c2c76245db6de1a
Xorag <adipasquale2190@gmail.com>
2021-01-27 21:19:56 -0500

    remove mount function

--- QuickCommands.lua
+++ QuickCommands.lua
@@ -7,27 +7,7 @@ function GreatVault() --Show the great vault UI anywhere
 	WeeklyRewardsFrame:Show()
 end
 
-function Mount() --Handy mount function for the maw and a favorite everywhere else in non-flying areas
-	in_instance, _ = IsInInstance()
-
-	if IsMounted() then
-		Dismount()
-	elseif GetZoneText() == "The Maw" then
-		C_MountJournal.SummonByID(1442) --Corridor Creeper
-	elseif IsFlyableArea() and not in_instance then
-		C_MountJournal.SummonByID(0) --Random Favorite
-	else
-		if math.random(1,100) <= 50 then
-			C_MountJournal.SummonByID(111) --Summon Zulian Tiger
-		else
-			C_MountJournal.SummonByID(1419) --Sintouched Deathwalker
-		end
-	end
-end
-
 SLASH_AR1 = '/ar'
 SLASH_GV1 = '/gv'
-SLASH_MNT1 = '/mnt'
 SlashCmdList["AR"] = AudioRestart
 SlashCmdList["GV"] = GreatVault
-SlashCmdList["MNT"] = Mount
